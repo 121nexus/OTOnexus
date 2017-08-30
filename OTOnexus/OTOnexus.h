c608a9c1ae975a59e3526cde15511cc632f111be
Chris DeOrio <chris@121nexus.com>
2017-08-30 11:53:44 -0400

    initial commit

---
+++ OTOnexus/OTOnexus.h
@@ -0,0 +1,19 @@
+//
+//  OTOnexus.h
+//  OTOnexus
+//
+//  Created by Christopher DeOrio on 8/30/17.
+//  Copyright © 2017 Christopher DeOrio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OTOnexus.
+FOUNDATION_EXPORT double OTOnexusVersionNumber;
+
+//! Project version string for OTOnexus.
+FOUNDATION_EXPORT const unsigned char OTOnexusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTOnexus/PublicHeader.h>
+
+
