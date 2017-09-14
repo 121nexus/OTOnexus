b12f17fc100fadc1eaaedf3c04a2467305731af9
Nic Schlueter <schlueter@gmail.com>
2017-09-14 12:17:02 -0400

    stubs out models

---
+++ OTOnexus/Classes/Models/Experience.swift
@@ -0,0 +1,16 @@
+//
+//  Experience.swift
+//  Pods
+//
+//  Created by Nicholas Schlueter on 9/14/17.
+//  Copyright © 2017 121Nexus. All rights reserved.
+//
+
+import Foundation
+
+class Experience {
+    var id = -1
+    var name = ""
+    var description = ""
+    var productRequired = false
+}
---
+++ OTOnexus/Classes/Models/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  Pods
+//
+//  Created by Nicholas Schlueter on 9/14/17.
+//  Copyright © 2017 121Nexus. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+    var url = ""
+    var defaultExperienceId = -1
+    var experiences = [Experience]()
+}
