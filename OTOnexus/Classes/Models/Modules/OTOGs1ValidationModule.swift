66347cd5a452d90f421859af7fca6cfd10ccabbd
Nic Schlueter <schlueter@gmail.com>
2017-10-05 15:29:06 -0400

    adds fake modules

---
+++ OTOnexus/Classes/Models/Modules/OTOGs1ValidationModule.swift
@@ -0,0 +1,11 @@
+//
+//  OTOGs1ValidationModule.swift
+//  OTOnexus
+//
+//  Created by Nicholas Schlueter on 10/5/17.
+//
+
+import Foundation
+
+public class OTOGs1ValidationModule : OTOModule {
+}
---
+++ OTOnexus/Classes/Models/Modules/OTOGudidModule.swift
@@ -0,0 +1,12 @@
+//
+//  OTOGudidModule.swift
+//  OTOnexus
+//
+//  Created by Nicholas Schlueter on 10/5/17.
+//
+
+import Foundation
+
+public class OTOGudidModule : OTOModule {
+
+}
