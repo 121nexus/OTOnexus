bf882a29305ae237e03f943d66da7f126a3f76a5
Nic Schlueter <schlueter@gmail.com>
2017-12-04 16:03:12 -0500

    adds constants file for example project configuration

---
+++ Example/OTOnexus/ApiConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  ApiConfiguration.swift
+//  OTOnexus_Example
+//
+//  Created by Nicholas Schlueter on 12/4/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+struct ApiConfiguration {
+    static let apiKey = "<YOUR API KEY HERE>"
+    /// Set the defaultExperienceId to the provided value
+    static let defaultExperienceId = -1
+}
