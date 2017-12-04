619aacac2f099f9fea10c8cd2a94e8581c7d0e13
Nic Schlueter <schlueter@gmail.com>
2017-12-04 10:34:16 -0500

    migrates docs back to main code line

---
+++ bin/build_docs.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+excluded = ["../*/OTOLookupActionResponse.swift","../*/OTOActionResponse.swift","../*/OTOReorderResponse.swift","../*/OTOSafetyCheckResponse.swift"]
+
+`jazzy --podspec ../OTOnexus.podspec \
+  --output ../docs \
+  --clean \
+  --config ../OTOnexus/.jazzy.yaml \
+  --theme fullwidth \
+  --exclude=#{excluded.join(",")}`
