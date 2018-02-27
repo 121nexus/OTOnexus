b9dd51ef1215df772598b433149c5505a0d1d9d7
Andrew McKnight <amcknight2718@gmail.com>
2018-02-27 11:55:02 -0700

    post notifications for user actions in built-in UI components
    track torch enabled/disabled state

---
+++ OTOnexus/Classes/Models/OTONotification.swift
@@ -0,0 +1,35 @@
+//
+//  OTONotification.swift
+//  OTOnexus
+//
+//  Created by Andrew McKnight on 2/27/18.
+//
+
+import Foundation
+
+public enum OTONotification: String {
+    case resetResults
+    case flashEnabled
+    case flashDisabled
+    case singleBarcode
+    case stackedBarcode
+
+    public init?(name: Notification.Name) {
+        if name.rawValue == OTONotification.resetResults.rawValue { self = .resetResults }
+        else if name.rawValue == OTONotification.flashEnabled.rawValue { self = .flashEnabled }
+        else if name.rawValue == OTONotification.flashDisabled.rawValue { self = .flashDisabled }
+        else if name.rawValue == OTONotification.singleBarcode.rawValue { self = .singleBarcode }
+        else if name.rawValue == OTONotification.stackedBarcode.rawValue { self = .stackedBarcode }
+        else { return nil }
+    }
+
+    public func name() -> Notification.Name {
+        switch self {
+        case .resetResults: return Notification.Name(OTONotification.resetResults.rawValue)
+        case .flashEnabled: return Notification.Name(OTONotification.flashEnabled.rawValue)
+        case .flashDisabled: return Notification.Name(OTONotification.flashDisabled.rawValue)
+        case .singleBarcode: return Notification.Name(OTONotification.singleBarcode.rawValue)
+        case .stackedBarcode: return Notification.Name(OTONotification.stackedBarcode.rawValue)
+        }
+    }
+}
